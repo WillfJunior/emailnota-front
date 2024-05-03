8ee516b0590c6b1c20888006adb4fe824a88a58f
willfjunior <wfrutuosojunior@gmail.com>
2024-05-03 15:15:35 -0300

    routes update

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Route, Routes as RoutesRoot } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import { Routes as RoutesRoot } from 'react-router'
 import Home from '../pages/Home.tsx';
 import ClientList from '../pages/Clientes/ClientList.tsx';
@@ -7,17 +7,17 @@ import AddClient from '../pages/Clientes/AddClient.tsx';
 import EditClient from '../pages/Clientes/EditClient.tsx';
 
 
-const Routes = () => {
+const Rotas = () => {
     return (
         <Router>
-            <RoutesRoot>
+            <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/list" element={<ClientList />} />
                 <Route path="/add" element={<AddClient />} />
                 <Route path="/edit/:id" element={<EditClient />} />
-            </RoutesRoot>
+            </Routes>
         </Router>
     );
 };
 
-export default Routes;
+export default Rotas;
